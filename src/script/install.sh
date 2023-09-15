55c22c49a5eefca4de89070733700c6c58284975
derrick921213 <dlin12457@gmail.com>
2023-09-15 12:47:17 +0800

    fix

--- src/script/install.sh
+++ src/script/install.sh
@@ -14,13 +14,4 @@ if [ "$?" != '0' ]; then
     echo "make Error"
     exit 1
 fi
-sudo activate-global-python-argcomplete3
-activate_pyzshcomplete
-cp ~/.zshrc ~/.zshrc.bak
-cp ~/.bashrc ~/.bashrc.bak
-sed -i '8i\export PATH="~/.local/bin:$PATH"' ~/.zshrc
-sed -i '9i\autoload -U +X bashcompinit;bashcompinit' ~/.zshrc
-sed -i '10i\eval "$(register-python-argcomplete3 dpm)"' ~/.zshrc
-echo 'export PATH="~/.local/bin:$PATH"'  >> ~/.bashrc
-echo 'eval "$(register-python-argcomplete dpm)"'>> ~/.bashrc
 echo [DPM] Install successful.
