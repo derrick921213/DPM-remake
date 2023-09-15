88163b895e66532e396913c4b5c88c029fb20ccb
derrick921213 <dlin12457@gmail.com>
2023-09-15 12:52:43 +0800

    fix

--- src/script/install.sh
+++ src/script/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #__author__:derrick921213
 cd /tmp
-sudo apt update -y && sudo apt install python3-venv patchelf software-properties-common python3-argcomplete -y
-sudo pip3 install pyzshcomplete
+sudo apt update -y && sudo apt install python3-venv patchelf software-properties-common git -y
+sudo pip3 install argcomplete pyzshcomplete
 git clone https://github.com/derrick921213/DPM-remake.git DPM_SRC
 if [ "$?" != '0' ]; then
     echo "git Error"
@@ -14,4 +14,14 @@ if [ "$?" != '0' ]; then
     echo "make Error"
     exit 1
 fi
+sudo activate-global-python-argcomplete3
+activate_pyzshcomplete
+cp ~/.zshrc ~/.zshrc.bak
+cp ~/.bashrc ~/.bashrc.bak
+sed -i '8i\export PATH="~/.local/bin:$PATH"' ~/.zshrc
+sed -i '9i\autoload -U +X bashcompinit;bashcompinit' ~/.zshrc
+sed -i '10i\autoload -U +X compinit && compinit' ~/.zshrc
+sed -i '11i\eval "$(register-python-argcomplete3 dpm)"' ~/.zshrc
+echo 'export PATH="~/.local/bin:$PATH"'  >> ~/.bashrc
+echo 'eval "$(register-python-argcomplete dpm)"'>> ~/.bashrc
 echo [DPM] Install successful.
