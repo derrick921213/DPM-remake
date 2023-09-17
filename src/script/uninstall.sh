0016b58c96c60742e224edf287e114bd36226aa7
derrick921213 <dlin12457@gmail.com>
2023-09-17 10:56:39 +0800

    更新uninstall.sh

--- src/script/uninstall.sh
+++ src/script/uninstall.sh
@@ -2,7 +2,4 @@
 [ ! -d /usr/local/DPM ] && echo [DPM NOT installed] && exit 1
 sudo rm -rf /usr/local/bin/dpm
 sudo rm -rf /usr/local/DPM
-rm -f ~/.bashrc ~/.zshrc
-mv ~/.zshrc.bak ~/.zshrc
-mv ~/.bashrc.bak ~/.bashrc
 echo [DPM] Uninstall successful.
