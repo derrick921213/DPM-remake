01513cbd2208aebcf8f8bd41397d7ce06ac51dbc
derrick921213 <dlin12457@gmail.com>
2023-09-30 11:38:52 +0800

    fix

--- src/script/uninstall.sh
+++ src/script/uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 [ ! -d /usr/local/DPM ] && echo [DPM NOT installed] && exit 1
-sudo rm -rf /usr/local/bin/dpm
+sudo rm -rf /usr/bin/dpm
 sudo rm -rf /usr/local/DPM
 sed -i '/eval "$(register-python-argcomplete3 dpm)"/d' .zshrc
 sed -i '/eval "$(register-python-argcomplete3 dpm)"/d' .bashrc
